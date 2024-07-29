7326a7139c437c4d92465a371f60dbbf0471a2ab
dcortesnet <dcortes.net@gmail.com>
2024-07-29 19:33:25 -0400

    primeras 2 rutas registros

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import psycopg2
 
 app = Flask(__name__)
+
+connection = psycopg2.connect(
+    dbname="pawpals",
+    user="postgres",
+    password="123456",
+    host="localhost"
+)
 
 @app.route('/')
 def home():
@@ -9,3 +17,41 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/register-pets-users', methods=['POST'])
+def register_pets_users():
+    body = request.get_json()
+    username = body.get('username')
+    email = body.get('email')
+    photo = 'https://static.scientificamerican.com/sciam/cache/file/E235EE4C-5D9A-44A3-9D0E30F0035C3151_source.jpg?w=1200'
+    password = body.get('password')
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT INTO pets_users (username, email, photo, password)
+        VALUES (%s, %s, %s, %s)""", [username, email, photo, password]
+    )
+    connection.commit()
+    cursor.close()
+    return jsonify({"message": "Usuario mascota creado correctamente"}), 201
+
+@app.route('/register-services-users', methods=['POST'])
+def register_services_users():
+    body = request.get_json()
+    username = body.get('username')
+    email = body.get('email')
+    photo = 'https://assets.petco.com/petco/image/upload/f_auto,q_auto:best/vet-services-vetco-total-care-lifestyle-img-800x577'
+    password = body.get('password')
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT INTO services_users (username, email, photo, password)
+        VALUES (%s, %s, %s, %s)""", [username, email, photo, password]
+    )
+    connection.commit()
+    cursor.close()
+    return jsonify({"message": "Usuario empresa creado correctamente"}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    return "Login"
+
+app.run(host='0.0.0.0', port=3000, debug=True)
---
+++ querys.sql
@@ -0,0 +1,8 @@
+INSERT INTO pets_users (username, email, photo, password)
+	VALUES ('gato1313', 'gato1313@gmail.com', 'https://static.scientificamerican.com/sciam/cache/file/E235EE4C-5D9A-44A3-9D0E30F0035C3151_source.jpg?w=1200', '123');
+
+INSERT INTO services_users (username, email, photo, password)
+	VALUES ('pepito1', 'pepito1@gmail.com', 'https://assets.petco.com/petco/image/upload/f_auto,q_auto:best/vet-services-vetco-total-care-lifestyle-img-800x577', '123');
+
+SELECT * FROM pets_users;
+SELECT * FROM services_users;
